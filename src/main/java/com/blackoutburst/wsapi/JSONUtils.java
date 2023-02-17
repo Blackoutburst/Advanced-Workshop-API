0032fbbb9d3197efcfbe3773aa28a8b392260829
Blackoutburst <blackoutburst@hotmail.com>
2023-02-17 20:38:01 +0100

    User route

---
+++ src/main/java/com/blackoutburst/wsapi/JSONUtils.java
@@ -0,0 +1,131 @@
+package com.blackoutburst.wsapi;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONUtils {
+
+    public static String generatePlayerListJSON() {
+        StringBuilder players = new StringBuilder();
+
+        File folder = new File("./plugins/Workshop/playerData");
+        File[] files = folder.listFiles();
+        if (files == null) return "{}";
+
+        for (int i = 0; i < files.length; i++) {
+            if (!files[i].isFile()) continue;
+
+            String[] fileName = files[i].getName().split("\\.");
+
+            if (fileName.length == 1) continue;
+            if (!fileName[1].equals("yml")) continue;
+
+            String uuid = fileName[0];
+            YamlConfiguration playerData = YamlConfiguration.loadConfiguration(files[i].getAbsoluteFile());
+            String name = playerData.getString("name");
+
+            players.append("{")
+                    .append("\"uuid\": \"").append(uuid).append("\",")
+                    .append("\"name\": \"").append(name).append("\"")
+                    .append("}");
+            if (i != files.length -1) {
+                players.append(",");
+            }
+        }
+
+        return "{" +
+                    "\"players\": [" +
+                        players +
+                    "]" +
+                "}";
+    }
+
+    public static String generatePlayerJSON(final File file, final String uuid) {
+        final YamlConfiguration playerData = YamlConfiguration.loadConfiguration(file);
+        String name = playerData.getString("name", "unknown");
+        int gameCount = playerData.getInt("gameCount", 0);
+        int roundCount = playerData.getInt("roundCount", 0);
+        List<String> maps = new ArrayList<>(playerData.getKeys(false));
+
+        int mapSize = maps.size();
+        for (int i = 0; i < mapSize; i++) {
+            String map = maps.get(i);
+            if (map.equals("name") || map.equals("gameCount") || map.equals("roundCount")) {
+                maps.remove(map);
+                mapSize--;
+                i--;
+            }
+        }
+
+        List<String> mapGameCount = new ArrayList<>();
+        for (String map : maps) {
+            int value = playerData.getInt(map + ".gameCount", 0);
+            mapGameCount.add("\"gameCount\": " + value);
+        }
+
+        List<String> mapRoundCount = new ArrayList<>();
+        for (String map : maps) {
+            int value = playerData.getInt(map + ".roundCount", 0);
+            mapRoundCount.add("\"roundCount\": " + value);
+        }
+
+        List<String> mapTime = new ArrayList<>();
+        for (String map : maps) {
+            double value = playerData.getDouble(map + ".time", 0);
+            mapTime.add("\"time\": " + value);
+        }
+
+        StringBuilder mapData = new StringBuilder();
+
+        for (int i = 0; i < maps.size(); i++) {
+            String mgc = mapGameCount.get(i);
+            String mrc = mapRoundCount.get(i);
+            String mt = mapTime.get(i);
+            String mn = maps.get(i);
+
+            StringBuilder craftData = new StringBuilder();
+
+            List<String> crafts = new ArrayList<>(playerData.getConfigurationSection(mn+".crafts").getKeys(false));
+
+            craftData.append("\"crafts\": [");
+
+            for (int j = 0; j < crafts.size(); j++) {
+                String craft = crafts.get(j);
+                double time = playerData.getDouble(mn+".crafts."+craft);
+                craftData.append("{")
+                        .append("\"name\": \"").append(craft).append("\",")
+                        .append("\"time\": ").append(time)
+                        .append("}");
+
+                if (j != crafts.size() -1) {
+                    craftData.append(",");
+                }
+            }
+            craftData.append("]");
+
+            mapData.append("{")
+                    .append("\"name\": \"").append(mn).append("\",")
+                    .append(mgc).append(",")
+                    .append(mrc).append(",")
+                    .append(mt).append(",")
+                    .append(craftData)
+                    .append("}");
+
+            if (i != maps.size() -1) {
+                mapData.append(",");
+            }
+        }
+
+        return "{" +
+                    "\"name\": \"" + name + "\"" + "," +
+                    "\"gameCount\":" + gameCount + "," +
+                    "\"roundCount\":" + roundCount + "," +
+                    "\"maps\": [" +
+                        mapData +
+                    "]" +
+                "}";
+    }
+}
--- src/main/java/com/blackoutburst/wsapi/Main.java
+++ src/main/java/com/blackoutburst/wsapi/Main.java
@@ -2,6 +2,7 @@ package com.blackoutburst.wsapi;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.eclipse.jetty.util.ajax.JSON;
 import spark.Spark;
 
 import java.io.File;
@@ -36,7 +37,7 @@ public class Main extends JavaPlugin {
                 return "Invalid token";
             }
 
-            return generatePlayerListJSON();
+            return JSONUtils.generatePlayerListJSON();
         });
 
         Spark.get("/user", (req, res) -> {
@@ -50,99 +51,17 @@ public class Main extends JavaPlugin {
 
             if (uuid == null) {
                 res.status(400);
-                return "Missing parameter 'uuid'";
+                return "Bad request";
             }
 
             final File file = new File("./plugins/Workshop/playerData/"+uuid+".yml");
             if (!file.exists()) {
-                return "null";
+                res.status(404);
+                return "Not found";
             }
 
-            return generatePlayerJSON(file, uuid);
+            return JSONUtils.generatePlayerJSON(file, uuid);
         });
 
-    }
-
-    private String generatePlayerListJSON() {
-
-        StringBuilder players = new StringBuilder();
-
-        File folder = new File("./plugins/Workshop/playerData");
-        File[] files = folder.listFiles();
-        if (files == null) return "{}";
-
-        for (int i = 0; i < files.length; i++) {
-            if (!files[i].isFile()) continue;
-
-            String[] fileName = files[i].getName().split("\\.");
-
-            if (fileName.length == 1) continue;
-            if (!fileName[1].equals("yml")) continue;
-
-            String uuid = fileName[0];
-            YamlConfiguration playerData = YamlConfiguration.loadConfiguration(files[i].getAbsoluteFile());
-            String name = playerData.getString("name");
-
-            players.append("{")
-                    .append("\"uuid\": \"").append(uuid).append("\",")
-                    .append("\"name\": \"").append(name).append("\"")
-                    .append("}");
-            if (i != files.length -1) {
-                players.append(",");
-            }
-        }
-
-        return "{" +
-            "\"players\": [" +
-                players +
-            "]" +
-        "}";
-    }
-
-    private String generatePlayerJSON(final File file, final String uuid) {
-        final YamlConfiguration playerData = YamlConfiguration.loadConfiguration(file);
-        final int Q = playerData.getInt("score.Q", 0);
-        final int F = playerData.getInt("score.F", 0);
-        final int WQ = playerData.getInt("score.WQ", 0);
-        final int L = playerData.getInt("score.L", 0);
-        final int WF = playerData.getInt("score.WF", 0);
-        final String name = playerData.getString("name");
-
-        String qh = "";
-        for (int i = 1; i <= 100; i++) qh += playerData.getInt("qualification_history."+i, 0)+",";
-        qh = qh.substring(0, qh.length() - 1);
-
-        String fh = "";
-        for (int i = 1; i <= 100; i++) fh += playerData.getInt("finals_history."+i, 0)+",";
-        fh = fh.substring(0, fh.length() - 1);
-
-        String wqh = "";
-        for (int i = 1; i <= 100; i++) wqh += playerData.getInt("wide_qualification_history."+i, 0)+",";
-        wqh = wqh.substring(0, wqh.length() - 1);
-
-        String lh = "";
-        for (int i = 1; i <= 100; i++) lh += playerData.getInt("lobby_history."+i, 0)+",";
-        lh = lh.substring(0, lh.length() - 1);
-
-        String wfh = "";
-        for (int i = 1; i <= 100; i++) wfh += playerData.getInt("wide_finals_history."+i, 0)+",";
-        wfh = wfh.substring(0, wfh.length() - 1);
-
-        return "{"
-                + "\"qualification_history\":["+qh+"],"
-                + "\"finals_history\":["+fh+"],"
-                + "\"wide_qualification_history\":["+wqh+"],"
-                + "\"lobby_history\":["+lh+"],"
-                + "\"wide_finals_history\":["+wfh+"],"
-                + "\"name\":\""+name+"\","
-                + "\"uuid\":\""+uuid+"\","
-                + "\"scores\":{"
-                + "\"qualification\":"+Q+","
-                + "\"finals\":"+F+","
-                + "\"wide_qualification\":"+WQ+","
-                + "\"lobby\":"+L+","
-                + "\"wide_finals\":"+WF
-                + "}"
-                + "}";
     }
 }
